528c07453764cc8c47aaef0304630aa3952061bd
fardinkhanfahmi <fardinkhanfahmi@gmail.com>
2026-07-05 23:17:48 +0600

    Add stack implementation using array in C

---
+++ Stack_using_array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define SIZE 5
+int stack[SIZE];
+int top = -1;
+void push(int value)
+{
+    if(top == SIZE - 1)
+    return;
+    stack[++top] = value;
+}
+void pop()
+{
+    if (top == -1)
+    return;
+    top--;
+}
+void display()
+{
+    for (int i = top; i >= 0; i--)
+    {
+        printf("%d ",stack[i]);
+    }
+}
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+    pop();
+    display();
+    return 0;
+}
